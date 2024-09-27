34fbe08ec14f3958efd7123190f82aae6700f1b7
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-27 13:25:03 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/springbootlecture2/SpringBootLecture2Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.springbootlecture2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLecture2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLecture2Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/springbootlecture2/SpringBootLecture2ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.springbootlecture2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLecture2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
