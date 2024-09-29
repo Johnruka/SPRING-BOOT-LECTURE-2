69dfbfaea55dd634e638aa73fc72e19717673a96
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-29 13:43:13 +0200

    Added getters, setters, constructors and relationship

--- src/main/java/se/lexicon/springbootlecture2/entity/Address.java
+++ src/main/java/se/lexicon/springbootlecture2/entity/Address.java
@@ -1,4 +1,37 @@
 package se.lexicon.springbootlecture2.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String street;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false, length = 6)
+    private String zipCode;
+
+    @OneToOne(mappedBy = "address" )
+    private Student student;
+
+    public Address(String street, String city, String zipCode) {
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
 }
