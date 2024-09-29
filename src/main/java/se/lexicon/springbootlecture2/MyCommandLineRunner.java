a37732431acdb7d047171c65a6024c775ee2c1b8
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-29 14:14:27 +0200

    Implemented

--- src/main/java/se/lexicon/springbootlecture2/MyCommandLineRunner.java
+++ src/main/java/se/lexicon/springbootlecture2/MyCommandLineRunner.java
@@ -1,4 +1,29 @@
 package se.lexicon.springbootlecture2;
 
-public class MyCommandLineRunner {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import se.lexicon.springbootlecture2.Repository.AddressRepository;
+import se.lexicon.springbootlecture2.Repository.StudentRepository;
+import se.lexicon.springbootlecture2.entity.Address;
+import se.lexicon.springbootlecture2.entity.Student;
+
+@Component
+public class MyCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Address address = new Address("Test Street", "Test City", "12345");
+        //Address savedAddress = addressRepository.save(address);
+        studentRepository.save(new Student("John", "Doe", "johndoe@test.se", address));
+
+
+    }
 }
